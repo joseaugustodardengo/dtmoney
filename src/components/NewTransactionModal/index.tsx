b5f3dc512d5fbcf4511c79b0860f824eabd09b32
joseaugustodardengo <joseaugusto.dardengo@gmail.com>
2021-03-31 19:07:05 -0300

    inserting new transaction into the list of transactions

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -19,15 +19,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionPr
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       type,
       category
     })
+
+    setTtitle('');
+    setAmount(0);
+    setCategory('');
+    setType('');
+    onRequestClose();
   }
 
   return (
